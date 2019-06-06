0eaeeb4529d37b6b78b85c916be487e407b116df
srey sokhom <41099990+SreySokhom@users.noreply.github.com>
2019-06-06 20:25:24 +0700

    connect some database
    create post table in database

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
+use DB;
 class FrontController extends Controller
 {
     public function index(){
     return view('frontView.home.homeContent');
     }
 
-
     public function showhome (){
-        return view('page.home');
+     return view('page.home');
     }
     public function showJoblist (){
-        return view('page.joblist');
+        $data=DB::table('posts')
+        ->join('conpany');
+        echo "<pre>";
+        print_r($data);
+
+        // $post= Post::all();
+        // return view('page.JobList')->with('posts',$post);
+       // return view('page.joblist');
     }
     public function showCategory (){
         return view('page.JobCategories');
--- resources/views/page/JobList.blade.php
+++ resources/views/page/JobList.blade.php
@@ -4,5 +4,18 @@
     <div style="margin-top: 100px">
         <h1>This is job list page</h1>
         <p>here is some example of the page</p>
+        {{--  @if(count($posts)>0)
+            @foreach ($posts as $post)
+                <div class="well">
+                <h3> {{$post->position}}</h3>
+                <small>write on {{$post->created_at}}</small>
+                </div>
+           @endforeach
+      @else
+        <p> No post found</p>
+      @endif  --}}
+                
+
+
     </div>
 @endsection
