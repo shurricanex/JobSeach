24c7a2597b05757edf5a9de65c6b673422629957
KIMDARA <kim.d.dara@gmail.com>
2019-07-04 21:54:03 +0700

    my update

--- resources/views/page/JobCategories.blade.php
+++ resources/views/page/JobCategories.blade.php
@@ -2,28 +2,30 @@
 
 @section('content')
 {{-- @extends('app.navbar') --}}
-<div class="container job_ca">
+<div class="container  bg-light p-4">
     <form action="/search2" method="get">
-        <table class="table table-hover">
+        <table class=" btn_cata" >
             <tr >
                 <td><a class="h1 text-success">Job Category</a></td>
-                <td></td>
+                 <td></td>
             </tr>
             <tr>
-                <td><input class="btn btn-light" type="submit" name="search" id="" value="account"></td> 
-                <td><input class="btn btn-light" type="submit" name="search" id="" value="engineer"></td>
+                <td ><input  type="submit" name="search" id="" value="account"> </td> 
+                <td><input  type="submit" name="search" id="" value="engineer"></td>
             </tr>
             <tr>
-                <td><input class="btn btn-light" type="submit" name="search" id="" value="teacher"></td>
-                <td><input class="btn btn-light" type="submit" name="search" id="" value="project manager"></td>
-            </tr>
-        </table>
-    </form>
+                <td> <input  type="submit" name="search" id="" value="teacher"></td>
+                <td> <input  type="submit" name="search" id="" value="project manager">
+                   
+               
+            </tr> 
+        </table> 
+    </form> 
     <h1>This is Category  pages</h1>
     <p>this is page of category of job</p>
 
-    </div>
-  <div>
+</div>
+<div>
   <a href="#" data-toggle="modal" data-target=".bd-example-modal-lg" >
 		<div class="container mt-3">
 			<div class="row">
@@ -46,33 +48,8 @@
 					
 			</div>
 		</div>
-	</a>
-      <!--		end of my design-->
-  <!--
-			   <div class="">
-				   <div class="row">
-					   <div class="col-9">
-	{{--                        fetch data --}}
-  @if(count($posts)>0)
-      @foreach ($posts as $post)
-          <div class="list_box">
-              <h3> <a href="/posts/{{$post->PID}}"> {{$post->position}}</a></h3>
-									   <p>write on {{$post->created_at}}</p>
-								   </div>
-							   @endforeach
-  @else
-      <p> Job not found</p>
-@endif
-
-      </div>
-
-  </div>
+	</a> --> -->
+      
 </div>
--->
-      <!--			</div>-->
-      <!--    </a>-->
-      <!--	</div>-->
-  </div>
-        </div>
 
 @endsection
